16aadb0b5f3e9958a235546ed7fbb6a9fccd4eb2
Vusal123 <vusal.eyvaz@gmail.com>
2018-10-11 15:38:41 +0400

    Updated complete.R

--- 02. Rprogramming/Week 2/complete.R
+++ 02. Rprogramming/Week 2/complete.R
@@ -18,23 +18,36 @@ complete<-function(directory, id = 1:332){
     directory<-("./specdata/")
   }
   
-  ##initialize result
+  ##initialize final matrix
   final_matrix<-matrix(ncol=2)
   colnames(final_matrix)<-c("id","nobs")
   
   ## find all files in the specdata folder
   all_files<-as.character(list.files(directory))
-  ##collect files to 
+  
+  ##collect files to file_path vector
   file_paths<-paste(directory, all_files, sep="")
   
+  
   for(i in id) {
-    data_vector<-c()
+  ##initialize data_vector
+	data_vector<-c()
+  ##run through files based on id and collect data to all_data vector
     current_file <- read.csv(file_paths[i], header=TRUE, sep=",")
     all_data<-current_file[,"sulfate"]
+	
+  ##remove NA values from the collected data
     data_vector <- na.omit(c(data_vector,all_data))
+  
+  ##collect id and nobs to final_matrix
 	final_matrix<-rbind(final_matrix,c(i,length(data_vector)))
   }
   final_matrix<-final_matrix[complete.cases(final_matrix), ]
-  result<-data.frame(id=final_matrix[,"id"],nobs=final_matrix[,"nobs"])
+  
+  if(NROW(final_matrix)==2){
+	result<-data.frame(id=final_matrix[["id"]],nobs=final_matrix[["nobs"]])
+  } else{
+	result<-data.frame(id=final_matrix[,"id"],nobs=final_matrix[,"nobs"])
+  }
   return(result)
 }
--- 02. Rprogramming/Week 2/pollutantmean.R
+++ 02. Rprogramming/Week 2/pollutantmean.R
@@ -11,27 +11,30 @@ pollutantmean<-function(directory, pollutant, id = 1:332){
   
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
-  ## NOTE: Do not roung the results!
   
   ##set working directory
   if(directory=="specdata"){
     directory<-("./specdata/")
   }
   
-  ##initialize mean value of the pollutant data
+  ##initialize vector for pollutant data
   data_vector<-c()
   
   ## find all files in the spec data
   all_files<-as.character(list.files(directory))
+  
   ##collect files to 
   file_paths<-paste(directory, all_files, sep="")
   
-  ##find mean for each id
+  ##collect required rollutant data to data_vector based on id
   for(i in id) {
     current_file <- read.csv(file_paths[i], header=TRUE, sep=",")
     polutant_data<-current_file[,pollutant]
     data_vector <- c(data_vector,polutant_data)
   }
+  ## remove NA values
   mean_vector<-na.omit(data_vector)
+  
+  ##return mean value
   return(mean(mean_vector))
 }
